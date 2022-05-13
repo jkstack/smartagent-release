21c37881eb9b5284d9c6138b4fdf9cde17e81982
lwch <lwch748@gmail.com>
2022-05-13 17:58:02 +0800

    修改release.go

--- go.mod
+++ go.mod
@@ -6,12 +6,14 @@ require (
 	github.com/gomarkdown/markdown v0.0.0-20220509074759-a57bf950ab8c
 	github.com/google/go-github v17.0.0+incompatible
 	github.com/lwch/runtime v0.0.0-20190520054850-8c97e19e0c6d
+	github.com/tdewolff/minify/v2 v2.11.2
 	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
 )
 
 require (
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/tdewolff/parse/v2 v2.5.29 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
--- release.go
+++ release.go
@@ -3,18 +3,24 @@ package main
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/ast"
+	"github.com/gomarkdown/markdown/html"
 	"github.com/gomarkdown/markdown/parser"
 	"github.com/google/go-github/github"
 	"github.com/lwch/runtime"
+	"github.com/tdewolff/minify/v2"
+	htmlmini "github.com/tdewolff/minify/v2/html"
 	"golang.org/x/oauth2"
 )
 
@@ -74,23 +80,29 @@ func createOrDrop(cli *github.Client, owner, repo, sha, version, body string) in
 	branch := "v" + version
 	version = "v" + version
 
+	log.Println("delete old tag...")
 	r, _ := cli.Git.DeleteRef(context.Background(), "jkstack", "smartagent", "tags/"+branch)
 	if r != nil {
 		defer r.Body.Close()
 	}
+
+	log.Println("delete old branch...")
 	r, _ = cli.Git.DeleteRef(context.Background(), "jkstack", "smartagent", "branches/"+branch)
 	if r != nil {
 		defer r.Body.Close()
 	}
+
 	rel, rep, err := cli.Repositories.GetReleaseByTag(context.Background(), owner, repo, branch)
 	if err == nil {
 		defer rep.Body.Close()
+		log.Println("old release found, delete...")
 		r, _ := cli.Repositories.DeleteRelease(context.Background(), owner, repo, rel.GetID())
 		if r != nil {
 			defer r.Body.Close()
 		}
 	}
 
+	log.Printf("created tag %s sha %s", version, sha)
 	msg := "auto create branch " + branch
 	t := "commit"
 	var tag github.Tag
@@ -100,11 +112,11 @@ func createOrDrop(cli *github.Client, owner, repo, sha, version, body string) in
 		Type: &t,
 		SHA:  &sha,
 	}
-	tg, resp, err := cli.Git.CreateTag(context.Background(), owner, repo, &tag)
+	_, resp, err := cli.Git.CreateTag(context.Background(), owner, repo, &tag)
 	runtime.Assert(err)
 	defer resp.Body.Close()
-	log.Printf("created tag %s sha %s", tg.GetTag(), sha)
 
+	log.Printf("create release %s...", version)
 	var release github.RepositoryRelease
 	release.TagName = &branch
 	release.Name = &version
@@ -142,6 +154,121 @@ func getChangeLog(version string) string {
 	data, err := ioutil.ReadFile("CHANGELOG.md")
 	runtime.Assert(err)
 
-	markdown.Parse(data, parser.New())
+	root := markdown.Parse(data, parser.New())
+	list := root.GetChildren()
+	if len(list) > 0 {
+		if !isChangeLog(list[0]) {
+			panic("is not CHANGELOG.md")
+		}
+	}
+	doc := new(ast.Document)
+	render := func() string {
+		data := markdown.Render(doc, html.NewRenderer(html.RendererOptions{}))
+		m := minify.New()
+		m.AddFunc("text/html", htmlmini.Minify)
+		dt, err := m.Bytes("text/html", data)
+		if err == nil {
+			return string(dt)
+		}
+		return string(data)
+	}
+	var nodes []ast.Node
+	var latest Version
+	for _, node := range list[1:] {
+		ver, ok := isVersion(node)
+		if ok {
+			if len(nodes) > 0 {
+				doc.SetChildren(nodes)
+				if latest.String() == version {
+					return render()
+				}
+			}
+			nodes = nodes[:0]
+			latest = ver
+		}
+		nodes = append(nodes, node)
+	}
+	if len(nodes) > 0 {
+		doc.SetChildren(nodes)
+		return render()
+	}
+
 	return ""
+}
+
+func getContent(node ast.Node) string {
+	contentToString := func(a, b []byte) string {
+		if len(a) > 0 {
+			return string(a)
+		}
+		if len(b) > 0 {
+			return string(b)
+		}
+		return ""
+	}
+	if c := node.AsContainer(); c != nil {
+		return contentToString(c.Literal, c.Content)
+	}
+	leaf := node.AsLeaf()
+	return contentToString(leaf.Literal, leaf.Content)
+}
+
+func isChangeLog(node ast.Node) bool {
+	if _, ok := node.(*ast.Heading); !ok {
+		return false
+	}
+	list := node.GetChildren()
+	if len(list) == 0 {
+		return false
+	}
+	return getContent(list[0]) == "CHANGELOG"
+}
+
+func isVersion(node ast.Node) (Version, bool) {
+	var ver Version
+	if _, ok := node.(*ast.Heading); !ok {
+		return ver, false
+	}
+	list := node.GetChildren()
+	if len(list) == 0 {
+		return ver, false
+	}
+	var err error
+	ver, err = ParseVersion(getContent(list[0]))
+	if err == nil {
+		return ver, true
+	}
+	return ver, false
+}
+
+type Version struct {
+	data [3]int
+}
+
+func ParseVersion(str string) (Version, error) {
+	var ret Version
+	tmp := strings.SplitN(str, ".", 3)
+	if len(tmp) != 3 {
+		return ret, errors.New("invalid version")
+	}
+	n, err := strconv.ParseInt(tmp[0], 10, 64)
+	if err != nil {
+		return ret, errors.New("invalid major version")
+	}
+	ret.data[0] = int(n)
+	n, err = strconv.ParseInt(tmp[1], 10, 64)
+	if err != nil {
+		return ret, errors.New("invalid minor version")
+	}
+	ret.data[1] = int(n)
+	n, err = strconv.ParseInt(tmp[2], 10, 64)
+	if err != nil {
+		return ret, errors.New("invalid patch version")
+	}
+	ret.data[2] = int(n)
+	return ret, nil
+}
+
+func (v Version) String() string {
+	return fmt.Sprintf("%d.%d.%d", v.data[0], v.data[1], v.data[2])
 }
